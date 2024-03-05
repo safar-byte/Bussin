7efa07f4a2d0c5e602197b40831311bb662e1ce3
glitch-head <118680463+glitch-head@users.noreply.github.com>
2024-03-05 14:26:00 +0530

    createJSON function
    input data to json

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,5 +1,41 @@
-document.getElementById("createButton").onclick = readData();
+//document.getElementById("submitBtn").onclick = readData();
 
 function readData(){
-    var busid = document.getElementById("BusId")   // BusID
+    var busid = document.getElementById("BusId").value;                 // BusID
+    var busType = document.getElementById("busType").value;             //BusType
+    var source = document.getElementById("source").value;               //source
+    var destination = document.getElementById("destination").value;    //destination value
+    var time = document.getElementById("time").value;                   //departure time
+
+createJSON(busid,busType ,source , destination , time)
+
+}
+
+//createJSON(1231, "superfast" , "TVM" , "KZD" , "8.00AM")
+
+function createJSON(id, type, source, destination, time){
+    var id = id;
+    var type = type;
+    var source = source;
+    var destination = destination;
+    var time = time;
+    var key = type;
+
+    var jsonObj = {
+        [key] : [
+            {
+                Busid : id,
+                type : type,
+                source : source, destination : destination,
+                time : time
+            }
+        ]
+    };
+
+    var jsonString = JSON.stringify(jsonObj);
+    const fs = require('fs');
+    fs.writeFile('mydata.json', jsonString, (err) => {
+        if (err) throw err;
+        console.log('Data saved');
+    })
 }
