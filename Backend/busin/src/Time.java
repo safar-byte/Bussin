2f8fb9d763464a84a1127974efea6e1bac25706a
sreedev <xreedev@gmail.com>
2024-03-05 17:43:46 +0530

    Changer csvreader

--- Backend/busin/src/Time.java
+++ Backend/busin/src/Time.java
@@ -1,33 +1,25 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Time {
-    public ArrayList<Bus> Filter(ArrayList<Bus> bustime ,String busArrival , String busDepature) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-        LocalTime StartTime = LocalTime.parse("9:00",formatter);
-        LocalTime endTime = LocalTime.parse("12:00",formatter);
-        LocalTime time =LocalTime.parse("HH:mm",formatter);
+    public ArrayList<Bus> Filter(ArrayList<Bus> bustime) {
+        LocalTime startTime = LocalTime.parse("09:00", DateTimeFormatter.ofPattern("HH:mm"));
+        LocalTime endTime = LocalTime.parse("12:00", DateTimeFormatter.ofPattern("HH:mm"));
 
-        ArrayList<Bus> timeShedule = new ArrayList<>();
-        for (Bus bus :bustime){
-            LocalTime arrive = LocalTime.parse(bus.getBusArrival() ,formatter);
-            LocalTime depature = LocalTime.parse(bus.getBusDepature() ,formatter);
-            if (arrive.isAfter(StartTime) && depature.isBefore(endTime)){
-                timeShedule.add(bus);
+        ArrayList<Bus> timeSchedule = new ArrayList<>();
+        for (Bus bus : bustime) {
+            String departureTimeStr = bus.getBusDepature().trim();
+            LocalTime departure = LocalTime.parse(departureTimeStr, DateTimeFormatter.ofPattern("HH:mm"));
+            if (departure.isAfter(startTime) && departure.isBefore(endTime)) {
+                timeSchedule.add(bus);
             }
-
         }
-        for(Bus hi:timeShedule) System.out.println(hi.getBusDepature()+hi.getBusName());
+
+        for (Bus bus : timeSchedule) {
+            System.out.println(bus.getBusDepature() + " " + bus.getBusName());
+        }
         System.out.println("FILE SCHEDULE CREATED SUCCESSFULLY");
-        return timeShedule;
-
-
-
-
+        return timeSchedule;
     }
 }
-
