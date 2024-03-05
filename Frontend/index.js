aaf52a92cf131b34001f50621fc84f8f455f1c2c
Fahad-Jaq <fahadsajeem11@gmail.com>
2024-03-05 13:52:32 +0530

    created createBtn js

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,5 +1,27 @@
-document.getElementById("createButton").onclick = readData();
+document.addEventListener("DOMContentLoaded", function () {
+    var createButton = document.getElementById("createButton");
+    var createForm = document.getElementById("createForm");
+    var midDiv = document.querySelector(".mid"); 
 
-function readData(){
-    var busid = document.getElementById("BusId")   // BusID
-}
+    createButton.addEventListener("click", function () {
+        toggleCreateForm();
+    });
+
+    function toggleCreateForm() {
+        if (createForm.style.display === "none" || createForm.style.display === "") {
+            createForm.style.display = "block";
+            midDiv.style.marginTop = "20px"; 
+        } else {
+            createForm.style.display = "none";
+            midDiv.style.marginTop = "0"; 
+        }
+    }
+});
+
+
+
+// document.getElementById("createButton").onclick = readData();
+
+// function readData(){
+//     var busid = document.getElementById("BusId")   // BusID
+// }
