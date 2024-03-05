793e65c42cabbf57e46e5017aa1afa6309eb4d93
sreedev <xreedev@gmail.com>
2024-03-05 14:44:11 +0530

    added the SourceDest.java

--- Backend/busin/src/SourceDest.java
+++ Backend/busin/src/SourceDest.java
@@ -1,7 +1,20 @@
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class SourceDest {
-        String filesrc="C:\\Users\\dell\\IdeaProjects\\Bussin\\CommonFiles\\mockdata.csv";
-         FileReader
 
+
+    public SourceDest(String filesrc) throws IOException {
+       FileReader f = new FileReader(filesrc);
+       BufferedReader br = new BufferedReader(f);
+{
+            String s;
+            while ((s = br.readLine()) != null) {
+
+                System.out.println(s);
+            }
+        }
+        br.close();
+    }
 }
