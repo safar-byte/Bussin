40ac9229134a76f626e139d5987d4f757cd044e7
Ashtami <ashtamiattu0612@gmail.com>
2024-03-05 16:32:54 +0530

    filter time

--- Backend/busin/src/Time.java
+++ Backend/busin/src/Time.java
@@ -6,9 +6,24 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Time {
-    public ArrayList<Bus> read(String path,String busArrival , String busDepature) {
+    public ArrayList<Bus> Filter(ArrayList<Bus> bustime ,String busArrival , String busDepature) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:MM");
+        LocalTime StartTime = LocalTime.parse("9:00",formatter);
+        LocalTime endTime = LocalTime.parse("12:00",formatter);
         LocalTime time =LocalTime.parse("HH:MM",formatter);
+
+        ArrayList<Bus> timeShedule = new ArrayList<>();
+        for (Bus bus :bustime){
+            LocalTime arrive = LocalTime.parse(bus.getBusArrival() ,formatter);
+            LocalTime depature = LocalTime.parse(bus.getBusDepature() ,formatter);
+            if (arrive.isAfter(StartTime) && depature.isBefore(endTime)){
+                timeShedule.add(bus);
+            }
+
+        }
+        return timeShedule;
+
+
 
 
     }
