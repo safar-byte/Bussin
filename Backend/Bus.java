554eb114f51c383390f4d65dc2fe3771a4260ba0
Muhammed Minhaju A <muhammedminhaj25@gmail.com>
2024-03-05 12:48:42 +0530

    Added common class and constructor is added

--- Backend/Bus.java
+++ Backend/Bus.java
@@ -1,3 +1,20 @@
 public class Bus{
+    private String busName;
+    private String busType;
+    private String busSource;
+    private String busDestination;
+    private String busArrival;
+    private String busDepature;
+
+
+    public Bus(String busName, String busType, String busSource, String busDestination, String busArrival, String busDepature) {
+        this.busName = busName;
+        this.busType = busType;
+        this.busSource = busSource;
+        this.busDestination = busDestination;
+        this.busArrival = busArrival;
+        this.busDepature = busDepature;
+    }
+
 
 }
