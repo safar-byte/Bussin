f85744a6935b5d3c98e91425b9b128bb7042bd9f
Muhammed Minhaju A <muhammedminhaj25@gmail.com>
2024-03-05 18:06:31 +0530

    added typeFilter and expectation

--- Backend/busin/src/CSVReader.java
+++ Backend/busin/src/CSVReader.java
@@ -1,25 +1,49 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class CSVReader {
 
-    String filesrc;
 
-    public ArrayList<Bus> CSVReader(String filesrc) throws IOException {
-     this.filesrc=filesrc;
-    }
-    public ArrayList<Bus> getArray() throws IOException {
+    public static ArrayList<Bus> getvalue(String filesrc) throws IOException {
         ArrayList<Bus> bus=new ArrayList<>();
-        java.io.FileReader f = new java.io.FileReader(filesrc);
+        FileReader f = new FileReader(filesrc);
         BufferedReader br = new BufferedReader(f);
         String s;
         while ((s = br.readLine()) != null) {
+            try {
+                String[] values = s.split(",");
+                if (values.length != 5) {
+                    throw new Exception("Invalid cvs file");
+                }
+                String busName = values[0].trim();
+                String busType = values[1].trim();
+                String busDestination = values[2].trim();
+                String busArrivals = values[3].trim();
+                String joinigdate = values[4].trim();
+                if (busName.isEmpty()) {
+                    throw new Exception("Employee id not found");
+                }
+                if (busType.isEmpty()) {
+                    throw new Exception("Employee name not found");
+                }
+                if (busDestination.isEmpty()) {
+                    throw new Exception("Employee designation not found");
+                }
+                if (busArrivals.isEmpty()) {
+                    throw new Exception("Employee salary not found");
+                }
+                if (joinigdate.isEmpty()) {
+                    throw new Exception("Employee joining date not found");
+                }
+                bus.add(new Bus(values[0], values[1], values[2], values[3], values[4], values[5]));
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+                System.exit(0);
 
-            String[] values = s.split(",");
-            bus.add(new Bus(values[0],values[1],values[2],values[3],values[4],values[5]));
-
+        }
         }
         return bus;
     }
---
+++ Backend/busin/src/typeFilter.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+public class typeFilter  {
+    public HashMap<String, Integer> hasfilterByType(List<Bus> buses) throws IOException {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for(Bus bus:buses){
+            hashMap.put(bus.getBusType(),hashMap.getOrDefault(bus.getBusType(), 0) + 1);
+
+        }
+        return hashMap;
+
+
+}
+}
