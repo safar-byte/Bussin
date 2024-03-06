664ce81654ddb33654571f3c166fb34f6c2b38b7
Muhammed Safar A <safarmuhmmed@gmail.com>
2024-03-06 12:35:19 +0530

    added the seatcount heading, readerclass and writer class is added

--- Backend/busin/src/CSVReader.java
+++ Backend/busin/src/CSVReader.java
@@ -17,27 +17,31 @@ public class CSVReader {
         while ((s = br.readLine()) != null) {
             try {
                 String[] values = s.split(",");
-                if (values.length != 5) {
+                if (values.length != 6) {
                     throw new Exception("Invalid cvs file");
                 }
                 String busName = values[0].trim();
                 String busType = values[1].trim();
-                String busDestination = values[2].trim();
-                String busArrivals = values[3].trim();
-                String joinigdate = values[4].trim();
+                String busSource = values[2].trim();
+                String busDestination = values[3].trim();
+                String busDeparture = values[4].trim();
+                String seatCount = values[5].trim();
                 if (busName.isEmpty()) {
                     throw new Exception("Employee id not found");
                 }
                 if (busType.isEmpty()) {
                     throw new Exception("Employee name not found");
                 }
-                if (busDestination.isEmpty()) {
+                if (busSource.isEmpty()) {
                     throw new Exception("Employee designation not found");
                 }
-                if (busArrivals.isEmpty()) {
+                if (busDestination.isEmpty()) {
                     throw new Exception("Employee salary not found");
                 }
-                if (joinigdate.isEmpty()) {
+                if (busDeparture.isEmpty()) {
+                    throw new Exception("Employee joining date not found");
+                }
+                if (seatCount.isEmpty()) {
                     throw new Exception("Employee joining date not found");
                 }
                 bus.add(new Bus(values[0], values[1], values[2], values[3], values[4], values[5]));
