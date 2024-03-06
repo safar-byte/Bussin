b36edf1e6aef41611aa8abf1c91986c6f09be2e6
Fahad-Jaq <fahadsajeem11@gmail.com>
2024-03-06 22:54:34 +0530

    updated create new button when clicked

--- Frontend/features/homepage/createForm.js
+++ Frontend/features/homepage/createForm.js
@@ -10,9 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
     function toggleCreateForm() {
         if (createForm.style.display === "none" || createForm.style.display === "") {
             createForm.style.display = "block";
+            createButton.textContent = 'Close'; 
+            createButton.style.backgroundColor= '#757575'
             adjustMidDivHeight(true); 
         } else {
             createForm.style.display = "none";
+            createButton.textContent = '+ Create New';
+            createButton.style.backgroundColor= '#425151'
             adjustMidDivHeight(false); 
         }
     }
