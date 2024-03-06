c7932e2c14aaa54aff36b0147655bd393790c77a
sreedev <xreedev@gmail.com>
2024-03-06 13:43:38 +0530

    Reservation class updated

--- Backend/busin/src/Reservation.java
+++ Backend/busin/src/Reservation.java
@@ -9,5 +9,35 @@ public class Reservation {
             uniqBusList.add(var.getBusName());
         }
         Set<String> uniqBusSet = new HashSet<>(uniqBusList);
+        for(String busname:uniqBusSet)
+        {
+            for(Reserve varRes:res)
+            {
+                if(varRes.getBusName().equals(busname))
+                {
+                    if(varRes.getAction().equals("Reserve"))
+                    {
+                        for(Bus newbus:bus)
+                        {
+                            if(newbus.getBusName().equals(varRes.getBusName()))
+                            {
+                                newbus.setSeatcnt(String.valueOf(Integer.parseInt(newbus.getSeatcnt())+1));
+                            }
+                        }
+                    }
+                    if(varRes.getAction().equals("Cancel"))
+                    {
+                        for(Bus newbus:bus)
+                        {
+                            if(newbus.getBusName().equals(varRes.getBusName()))
+                            {
+                                newbus.setSeatcnt(String.valueOf(Integer.parseInt(newbus.getSeatcnt())-1));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
