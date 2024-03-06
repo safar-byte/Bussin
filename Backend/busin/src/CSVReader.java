8c61203efec741f65d9676f20dac1b5ca51dd3c5
Muhammed Safar A <safarmuhmmed@gmail.com>
2024-03-06 13:43:59 +0530

    read class is updated

--- Backend/busin/src/CSVReader.java
+++ Backend/busin/src/CSVReader.java
@@ -13,7 +13,7 @@ public class CSVReader {
         ArrayList<Bus> bus=new ArrayList<>();
         FileReader f = new FileReader(filesrc);
         BufferedReader br = new BufferedReader(f);
-        String s;
+        String s = br.readLine(); //skip first line
         while ((s = br.readLine()) != null) {
             try {
                 String[] values = s.split(",");
@@ -27,22 +27,22 @@ public class CSVReader {
                 String busDeparture = values[4].trim();
                 String seatCount = values[5].trim();
                 if (busName.isEmpty()) {
-                    throw new Exception("Employee id not found");
+                    throw new Exception("Bus name not found");
                 }
                 if (busType.isEmpty()) {
-                    throw new Exception("Employee name not found");
+                    throw new Exception("Bus Type not found");
                 }
                 if (busSource.isEmpty()) {
-                    throw new Exception("Employee designation not found");
+                    throw new Exception("Bus Source not found");
                 }
                 if (busDestination.isEmpty()) {
-                    throw new Exception("Employee salary not found");
+                    throw new Exception("Bus Destination not found");
                 }
                 if (busDeparture.isEmpty()) {
-                    throw new Exception("Employee joining date not found");
+                    throw new Exception("Bus Departure date not found");
                 }
                 if (seatCount.isEmpty()) {
-                    throw new Exception("Employee joining date not found");
+                    throw new Exception("Seat Count not found");
                 }
                 bus.add(new Bus(values[0], values[1], values[2], values[3], values[4], values[5]));
             }catch (Exception e){
