458ba621b151d1219b95c9993965ba50a5dd1b11
glitch-head <118680463+glitch-head@users.noreply.github.com>
2024-03-07 11:21:25 +0530

    Updated formTable
    Seatcout validation

--- Frontend/common/formTable.js
+++ Frontend/common/formTable.js
@@ -32,33 +32,39 @@ function addData() {
       
 
 //////////////////////////////////////////////////////////////
-
+    let count = parseInt(seatCount)
     if(source == destination){  //source - destination validation
       window.alert("Your source and destination are same!!")
     }else{
       if(is_unique){ //busname validation
-        charSet.add(busName);
-        console.log("different")
-        let row = tablerow.insertRow(-1);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
-        let cell4 = row.insertCell(3);
-        let cell5 = row.insertCell(4);
-        let cell6 = row.insertCell(5);
-        
-        cell1.innerHTML = busName;
-        cell2.innerHTML = busType;
-        cell3.innerHTML = source;
-        cell4.innerHTML = destination;
-        cell5.innerHTML = time;
-        cell6.innerHTML = seatCount;
+        console.log(seatCount)
+        if(count >= 30 && count <= 60){
+
+          charSet.add(busName);
+          console.log("different")
+          let row = tablerow.insertRow(-1);
+          let cell1 = row.insertCell(0);
+          let cell2 = row.insertCell(1);
+          let cell3 = row.insertCell(2);
+          let cell4 = row.insertCell(3);
+          let cell5 = row.insertCell(4);
+          let cell6 = row.insertCell(5);
+          
+          cell1.innerHTML = busName;
+          cell2.innerHTML = busType;
+          cell3.innerHTML = source;
+          cell4.innerHTML = destination;
+          cell5.innerHTML = time;
+          cell6.innerHTML = seatCount;
+          document.getElementById("form").reset();
+        }else{
+          window.alert("Maximum seat count : 60 \nMinimum seat count : 30")
+        }
       }
       else{
         window.alert('Bus name already exists!!')
       }
 
-      document.getElementById("form").reset();
     }
 
 
