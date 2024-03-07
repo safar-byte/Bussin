a765ace0554a50466797c9977da291fdf03c01f6
sreedev <xreedev@gmail.com>
2024-03-07 12:45:29 +0530

    reservation updated

--- Backend/busin/src/Reservation.java
+++ Backend/busin/src/Reservation.java
@@ -3,41 +3,62 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Reservation {
-    public void seatUpdater( ArrayList<Bus> bus ,ArrayList<Reserve> res) {
-        Set<String> uniqBusSet = new HashSet<>();
-        for (Reserve var : res) {
-            uniqBusSet.add(var.getBusName());
-        }
-//added
-        for(String busname:uniqBusSet)
+//    public void seatUpdater( ArrayList<Bus> bus ,ArrayList<Reserve> res) {
+//        Set<String> uniqBusSet = new HashSet<>();
+//        for (Reserve var : res) {
+//            uniqBusSet.add(var.getBusName());
+//        }
+////added
+//        for(String busname:uniqBusSet)
+//        {
+//            for(Reserve varRes:res)
+//            {
+//                if(varRes.getBusName().equals(busname))
+//                {
+//                    if(varRes.getAction().equals("Reserve"))
+//                    {
+//                        for(Bus newbus:bus)
+//                        {
+//                            if(newbus.getBusName().equals(varRes.getBusName()))
+//                            {
+//                                newbus.setSeatcnt(String.valueOf(Integer.parseInt(newbus.getSeatcnt())-1));
+//                            }
+//                        }
+//                    }
+//                    if(varRes.getAction().equals("Leave"))
+//                    {
+//                        for(Bus newbus:bus)
+//                        {
+//                            if(newbus.getBusName().equals(varRes.getBusName()))
+//                            {
+//                                newbus.setSeatcnt(String.valueOf(Integer.parseInt(newbus.getSeatcnt())+1));
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//    }
+
+    public void seatUpdater(ArrayList<Bus> bus ,ArrayList<Reserve> res)
+    {
+        for(Reserve varRes :res)
         {
-            for(Reserve varRes:res)
+            if(varRes.getAction().equals("reserve"))
             {
-                if(varRes.getBusName().equals(busname))
-                {
-                    if(varRes.getAction().equals("Reserve"))
-                    {
-                        for(Bus newbus:bus)
-                        {
-                            if(newbus.getBusName().equals(varRes.getBusName()))
-                            {
-                                newbus.setSeatcnt(String.valueOf(Integer.parseInt(newbus.getSeatcnt())-1));
-                            }
-                        }
-                    }
-                    if(varRes.getAction().equals("Leave"))
-                    {
-                        for(Bus newbus:bus)
-                        {
-                            if(newbus.getBusName().equals(varRes.getBusName()))
-                            {
-                                newbus.setSeatcnt(String.valueOf(Integer.parseInt(newbus.getSeatcnt())+1));
-                            }
-                        }
-                    }
+                for(Bus newbus:bus) {
+                    newbus.setSeatcnt(String.valueOf(Integer.parseInt(newbus.getSeatcnt()) - 1));
+                    newbus.setReserve(newbus.getReserve()+1);
+                }
+            }
+            if(varRes.getAction().equals("cancel"))
+            {
+                for(Bus newbus:bus) {
+                    newbus.setSeatcnt(String.valueOf(Integer.parseInt(newbus.getSeatcnt())+1));
+                    newbus.setReserve(newbus.getCancel()+1);
                 }
             }
         }
-
     }
 }
